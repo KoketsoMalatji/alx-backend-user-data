21f15cd392ea21315517c8a5453dc4c11bfd9c56
Koketso Malatji <koketsosean209@gmail.com>
2023-02-02 01:02:00 +0200

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-""" Implement an is_valid function that expects 2 arguments and returns a boolean. """
+""" Use the bcrypt package to perform the hashing (with hashpw)
+"""
+
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Takes in string arg, converts to unicode
-    Returns salted, hashed pswd as bytestring
+    """ Salted pass generation
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Checks if hashed and unhashed pswds are same
-    Returns bool
+    """ is valid?
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
